ca3a18450bf3760ddcfe0256a05fd1e880828759
euanfblair <euanfblair@gmail.com>
2024-07-30 10:50:09 +0100

    test suite refactored

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMesssage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMesssage(t, got, want)
+	})
+
+}
+
+func assertCorrectMesssage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
