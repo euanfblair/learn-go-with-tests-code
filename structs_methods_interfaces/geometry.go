ae4c90b39e0966f86b0b2beadb1ce0ded12b0e5e
euanfblair <euanfblair@gmail.com>
2024-08-01 13:22:17 +0100

    Structs chapter

---
+++ structs_methods_interfaces/geometry.go
@@ -0,0 +1,24 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectanlge Rectangle) float64 {
+	return 2 * (rectanlge.Width + rectanlge.Height)
+}
